f6bd7bbb65d543442668de4297a72e189b30a569
Nathan Kimpel <nathan.kimpel@engelvoelkers.com>
2020-10-12 13:34:12 -0500

    Nathan Test

---
+++ .vscode/first app.js
@@ -0,0 +1,10 @@
+console.log(5);
+console.log('natedog');
+let age = 7;
+//comment note
+/*this is a big line comment 
+on multi lines
+*/
+let single = `nathan test of string ${age} print`;
+console.log(single);
+console.log(`Nathan is ${40+5} years old`);
